afa20f1726f7de328d15a176f4ed437d8b78dfec
nikinov <54107324+nikinov@users.noreply.github.com>
2023-05-05 22:11:58 +0200

    more cmake and setup stuff

--- fc_base-Clang/rnnoise-src/external/FST/tests/c_test.c
+++
@@ -1,5 +0,0 @@
-#include "fst/fst.h"
-
-int main(void) {
-    return 0;
-}
--- fc_base-Clang/rnnoise-src/external/FST/tools/hexdump2bin.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-for fname in sys.argv[1:]:
-  with open(fname) as f:
-    for line in f.readlines():
-        data = bytes([int(_,16) for _ in line.split()])
-        sys.stdout.buffer.write(data)
